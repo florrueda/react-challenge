c478c17296e9ee410d894b8763c8f3f3839db6b3
Flor-Rueda <flor.rueda@gmail.com>
2023-05-08 15:11:48 -0300

    Arreglo toggler

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -23,7 +23,7 @@ const Header = (props) => {
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                   <Link to="/" className="nav-link">
                     Home
